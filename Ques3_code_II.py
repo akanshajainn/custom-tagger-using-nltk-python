47b464f099eddf142ce34e969c2bfbc362b03288
Akansha Jain <akansha.da1@iiitmk.ac.in>
2017-11-24 18:25:45 +0530

    Add files via upload

---
+++ Ques3_code_II.py
@@ -0,0 +1,10 @@
+#One solution is to create a manual UnigramTagger that backs off to the NLTK tagger. Something like this:
+
+import nltk.tag, nltk.data
+path='/Users/akanshajain/nltk_data/taggers/maxent_treebank_pos_tagger/english.pickle'
+default_tagger = nltk.data.load(path)
+model = {'select': 'VB'}
+tagger = nltk.tag.UnigramTagger(model=model, backoff=default_tagger)
+#Then you get
+tagger.tag(['select', 'the', 'files'])
+#[('select', 'VB'), ('the', 'DT'), ('files', 'NNS')]
