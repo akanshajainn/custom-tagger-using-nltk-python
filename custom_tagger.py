2511bc05b9d1b45127de2cb5ba9fc11b6bc64922
Akansha Jain <akansha.da1@iiitmk.ac.in>
2018-02-07 15:53:39 +0530

    Update custom_tagger.py

--- custom_tagger.py
+++ custom_tagger.py
@@ -1,10 +1,15 @@
 #One solution is to create a manual UnigramTagger that backs off to the NLTK tagger. Something like this:
 
+#importing dependencies
 import nltk.tag, nltk.data
-path='/Users/akanshajain/nltk_data/taggers/maxent_treebank_pos_tagger/english.pickle'
+#path to the already existing tagger: nltk_data/taggers/maxent_treebank_pos_tagger/english.pickle
+#Find it by searching in your computer where you downloaded the nltk package.
+#If not found then download from here http://www.nltk.org/data.html and then give path to english.pickle
+path='PUT YOUR PATH HERE'
 default_tagger = nltk.data.load(path)
-model = {'select': 'VB'} #add your custom tags
+model = {'select': 'VB', 'important': 'JJ'} #add your custom tags
 tagger = nltk.tag.UnigramTagger(model=model, backoff=default_tagger)
+#Input to test
+tagger.tag(['select', 'the', 'important', 'files'])
 #Then you get
-tagger.tag(['select', 'the', 'files'])
-#[('select', 'VB'), ('the', 'DT'), ('files', 'NNS')]
+#[('select', 'VB'), ('the', 'DT'), ('important', 'JJ'), ('files', 'NNS')]
